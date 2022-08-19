26927daeb8d33f550087603c0486ab0199bccfb9
Aegis020404 <Aegis020404@gmail.com>
2022-08-19 21:18:44 +0300

    add page 404

--- pages/_offline.js
+++ pages/_offline.js
@@ -1,5 +1,7 @@
 import Head from 'next/head'
 import cl from '/src/style/offline.module.css'
+import MyBtnFiled from "../src/componentsUI/UI/buttonback/MyBtnFiled";
+import React from "@types/react";
 
 export default () => (
     <>
@@ -8,9 +10,12 @@ export default () => (
         </Head>
         <div className={cl.container}>
             <img src="/icon-256x256.png" alt=""/>
-            <h1>Данная страница OFFLINE!</h1>
+            <h1 className={cl.title}>Данная страница OFFLINE!</h1>
             <h2>У вас отключен интернет, посетите данную страницу с
                 включенным интернетом, после этого она будет работать offline</h2>
+            <a className={cl.href} href="/">
+                <MyBtnFiled classes={cl.btn} >На домашнею страницу</MyBtnFiled>
+            </a>
         </div>
     </>
 )
