6d49066496cf5133c00e2fee5e695f94abdaad9b
Aegis020404 <Aegis020404@gmail.com>
2022-08-19 20:05:12 +0300

    check offline page2

--- pages/_offline.js
+++ pages/_offline.js
@@ -1,13 +1,16 @@
 import Head from 'next/head'
+import cl from '/src/style/offline.module.css'
 
 export default () => (
     <>
         <Head>
-            <title>OFFLINE</title>
+            <title>it-industrial</title>
         </Head>
-        <img src="/icon-512x512.png" alt=""/>
-        <h1>Данная страница OFFLINE!</h1>
-        <h2>У вас отключен интернет, посетите данную страницу с
-             включенным интернетом, после этого она будет работать offline</h2>
+        <div className={cl.container}>
+            <img src="/icon-256x256.png" alt=""/>
+            <h1>Данная страница OFFLINE!</h1>
+            <h2>У вас отключен интернет, посетите данную страницу с
+                включенным интернетом, после этого она будет работать offline</h2>
+        </div>
     </>
 )
