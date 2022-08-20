946148606a67edefaedee147bb76ed7a45293fa4
BuildTools <unconfigured@null.spigotmc.org>
2022-08-21 02:15:03 +0300

    implement backend initial state get and set. Set has design flaws, must be disscussed.

---
+++ pages/api/state/set/index.js
@@ -0,0 +1,25 @@
+var db = require("../../../../src/API/DataStore").db;
+
+export default async (req, res) => {
+    let category = req.query.category;
+    if(typeof category !== 'string') {
+        res.status(400).json({err: "category invalid!"});
+        return;
+    }
+
+    let id = req.query.id;
+    if(id === undefined) {
+        res.status(400).json({err: "id invalid!"});
+        return;
+    }
+
+    let data = req.query.data;
+    if(data === undefined) {
+        res.status(400).json({err: "data invalid!"});
+        return;
+    }
+
+
+    db.push("/" + category + "/" + id + "/", data);
+    res.status(200).json({status: "ok"});
+}
