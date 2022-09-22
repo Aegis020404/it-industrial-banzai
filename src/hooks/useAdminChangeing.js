28f39a09eb6945fec281f5a079ef6b1c56f21d50
Shmeb <1gledpobgornuk@gmail.com>
2022-09-22 19:50:45 +0300

    update-url-error

--- src/hooks/useAdminChangeing.js
+++ src/hooks/useAdminChangeing.js
@@ -1,11 +1,11 @@
 
 export const useFetchingPost = async (infoChange, category, id,isAdmin)=>{
    if( id !== undefined) {
-    const response = await fetch(`api/state/set?category=${category}&id=${id}&data=${JSON.stringify(infoChange)}`)
+    const response = await fetch(`/api/state/set?category=${category}&id=${id}&data=${JSON.stringify(infoChange)}`)
     const data = await response.json()
     return data
    }
-   const response = await fetch(`api/state/set?category=adminTexts&id=1&data=${JSON.stringify({ads:1})}`)
+   const response = await fetch(`/api/state/set?category=adminTexts&id=1&data=${JSON.stringify({ads:1})}`)
    const data = await response.json()
    return data
 
@@ -13,7 +13,7 @@ export const useFetchingPost = async (infoChange, category, id,isAdmin)=>{
  }
 
 export const useFetchingGet = async (urlApi)=>{
-    const response = await fetch('api/state/get/'+urlApi)
+    const response = await fetch('/api/state/get'+urlApi)
     const data = await response.json()
     if( typeof data === 'object') {
         return data
@@ -23,7 +23,7 @@ export const useFetchingGet = async (urlApi)=>{
 }
 
 export const useFetchingImg = async (formData)=>{
-    const response = await fetch(`api/upload`, {
+    const response = await fetch(`/api/upload`, {
         method: 'POST',
         body: formData,
     })
@@ -33,7 +33,7 @@ export const useFetchingImg = async (formData)=>{
 
 
  export const usePing = async ()=>{
-    const response = await fetch(`api/ping`)
+    const response = await fetch(`/api/ping`)
     const data = await response.json()
     
     return data
